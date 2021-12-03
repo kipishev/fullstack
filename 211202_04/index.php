65857f30d83f73871cecf059a5130d7f56cb8fb2
tksrei <tiki@list.ru>
2021-12-03 15:39:49 +0300

    Standarts

---
+++ 211202_04/index.php
@@ -0,0 +1,14 @@
+<?php
+echo "Hello, world!";
+echo '<br>';
+echo 'Hello!';
+echo '<br>';
+echo 55;
+echo '<br>';
+echo '<button>CLICK!</button>';
+echo '<br>';
+echo date('d.m.Y H:i:s');
+echo '<br>';
+echo 2 ** 10; /*возведение в степень*/
+echo '<br>';
+echo 11 % 3; /*остаток от деления*/
